52b0be86ee6a8e13b4d2256224d64c26303ef3f3
Daniel <d.mckibbin@2016.ljmu.ac.uk>
2020-02-11 19:59:39 +0000

    Network Models created

---
+++ Audit Scanner/Network/Models/DeviceModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Audit_Scanner.Network.Models
+{
+    public class DeviceModel
+    {
+        public string DeviceName { get; set; }
+        public List<ServiceModel> Services { get; set; }
+    }
+}
---
+++ Audit Scanner/Network/Models/ServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Audit_Scanner.Network.Models
+{
+    public class ServiceModel
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public bool Vulnerable { get; set; }
+        public VulnerabilityModel Vulnerability { get; set; }
+    }
+}
