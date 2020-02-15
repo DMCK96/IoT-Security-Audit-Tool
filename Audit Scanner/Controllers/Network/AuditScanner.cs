09909b81cff01ec43636dc14a3b7bcc3c6a87d9e
Daniel <d.mckibbin@2016.ljmu.ac.uk>
2020-02-15 16:52:18 +0000

    Started vulnerability API client, refactored hostdiscover

---
+++ Audit Scanner/Controllers/Network/AuditScanner.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Audit_Scanner.Controllers.Vulnerability;
+using Audit_Scanner.Helper;
+using Audit_Scanner.Network.Models;
+using RestSharp;
+using SaltwaterTaffy;
+using SaltwaterTaffy.Container;
+
+namespace Audit_Scanner.Network
+{
+    public class AuditScanner
+    {
+        public List<DeviceModel> Devices { get; } = new List<DeviceModel>();
+
+        public List<Host> HostDiscover(string ip, bool known, string range = "24")
+        {
+            Target target;
+            var foundHosts = new List<Host>();
+            
+            if (known)
+            {
+                target = new Target(ip);
+                foundHosts.AddRange(new Scanner(target).HostDiscovery());
+            }
+            else
+            {
+                var localIpAddresses = NetworkInterfaceHelper.GetLocalIPAddresses();
+
+                foreach (var address in localIpAddresses)
+                {
+                    target = new Target($"{address}/{range}");
+                    foundHosts.AddRange(new Scanner(target).HostDiscovery());
+                }
+            }
+            
+            return foundHosts;
+        }
+        
+        public List<DeviceModel> VulnerabilityScan(List<DeviceModel> devices)
+        {
+            var client = new VulnerabilityClient();
+
+            /*foreach (var device in result)
+            {
+                var singleDevice = new DeviceModel();
+                singleDevice.Address = device.Address;
+                singleDevice.Name = device.OsMatches.FirstOrDefault().Name;
+                singleDevice.OpenPorts = device.Ports.ToList();
+
+                foreach (var port in device.Ports)
+                {
+                    var singleService = new ServiceModel();
+                    singleService.Name = port.Service.Name;
+                    singleService.Version = port.Service.Version;
+                    singleService.Port = port.PortNumber.ToString();
+                }
+
+                Devices.Add(singleDevice);
+            }*/
+            
+            return client.ScanVulnerabilities(devices);
+        }
+    }
+}
---
+++ Audit Scanner/Helper/NetworkInterfaceHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Audit_Scanner.Helper
+{
+    public class NetworkInterfaceHelper
+    {
+        public static List<string> GetLocalIPAddresses()
+        {
+            var list = new List<string>();
+
+            var adapters = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (var adapter in adapters)
+            {
+                var unicastAddresses = adapter.GetIPProperties().UnicastAddresses;
+
+                foreach (var address in unicastAddresses)
+                {
+                    if (address.PrefixLength == 24)
+                    {
+                        var ipString = address.Address.ToString();
+                        var updated = ipString.Substring(0, ipString.LastIndexOf(".", StringComparison.Ordinal) + 1);
+                        updated = updated + "0";
+                        list.Add(updated);
+                    }
+                }
+            }
+            
+            return list;
+        }
+    }
+}
